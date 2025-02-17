229e845d033cfd2ee99dfadda0424fc515add6ba
Sukhmeet Singh Hora <maherhora468@gmail.com>
2025-02-16 20:06:28 -0600

    project updates completes - last course (not attempted the challenge)

--- src/components/contest.tsx
+++ src/components/contest.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
-import { fetchContest } from "../api-client";
+import {
+  addNewNameToContest,
+  fetchContest,
+} from "../api-client";
 
 import Header from "./header";
 
@@ -20,12 +23,49 @@ const Contest = ({ initialContest, onContestListClick }) => {
     onContestListClick();
   };
 
+  const handleNewNameSubmit = async (event) => {
+    event.preventDefault();
+    const newNameInput = event.target.newName;
+    const updatedContest = await addNewNameToContest({
+      contestId: contest.id,
+      newNameValue: newNameInput.value,
+    });
+    setContest(updatedContest);
+  };
+
   return (
     <>
       <Header message={contest.contestName} />
       <div className="contest">
         <div className="title">Contest Description</div>
         <div className="description">{contest.description}</div>
+
+        <div className="title">Proposed Names</div>
+        <div className="body">
+          {contest.names?.length > 0 ? (
+            <div className="list">
+              {contest.names.map((proposedName) => (
+                <div key={proposedName.id} className="item">
+                  {proposedName.name}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No names proposed yet</div>
+          )}
+        </div>
+
+        <div className="title">Propose a New Name</div>
+        <div className="body">
+          <form onSubmit={handleNewNameSubmit}>
+            <input
+              type="text"
+              name="newName"
+              placeholder="New Name Here.."
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
 
         <a
           href="/"
--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -5,6 +5,7 @@ import { connectClient } from "./db";
 
 const router = express.Router();
 router.use(cors());
+router.use(express.json());
 
 router.get("/contests", async (req, res) => {
   const client = await connectClient();
@@ -31,6 +32,30 @@ router.get("/contest/:contestId", async (req, res) => {
     .findOne({ id: req.params.contestId });
 
   res.send({ contest });
+});
+
+router.post("/contest/:contestId", async (req, res) => {
+  const client = await connectClient();
+
+  const { newNameValue } = req.body;
+
+  const doc = await client
+    .collection("contests")
+    .findOneAndUpdate(
+      { id: req.params.contestId },
+      {
+        $push: {
+          names: {
+            id: newNameValue.toLowerCase().replace(/\s/g, "-"),
+            name: newNameValue,
+            timestamp: new Date(),
+          },
+        },
+      },
+      { returnDocument: "after" },
+    );
+
+  res.send({ updatedContest: doc.value });
 });
 
 export default router;
